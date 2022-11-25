b9b2de68ef7af5c9d737801cd189415847175fbb
Danny Bracht <brachtdanny@gmail.com>
2022-11-25 14:19:25 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -9,82 +9,138 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score*/
-//create variables 
-//document.querySelector
+
+//create variables
+//document.querySelector /.getElementById
 var startButton = document.querySelector(".start-button");
 var score = document.querySelector(".highscores");
-var questions = document.querySelector("qa-container");
-var timer = document.querySelector(".timer")
+var questionsEl = document.querySelector("#questionsEl");
+var timer = document.querySelector(".timer");
+var choices = document.querySelector(".choices");
+var result = document.querySelector(".result");
+var result = document.querySelector(".showAnswer");
+var savedInitials = document.getElementById("#save-initials");
+var scoreCount = document.getElementById("#saved-score");
+var saveButton = document.getElementById("#save-button");
+
 //quiz variables
-
-var showQuestion = [];
+var timerCount = 30;
+//var showQuestion = [];
 var questions = [
-    {
+  {
     question: "Javascript is a ____-side programming language",
-    answers: [
-        {text: "Client", correct: false,
-        text: "Server", correct: false,
-        text: "Both", correct: true,
-        text: "None", correct: false}
-    ]
+    choices: ["Client", "Server", "Both", "None"],
+    correctAnswer: "Both",
+  },
+  {
     question: "How do you find the min of x and y using Javascript?",
-    answers: [
-	{text: "min(x,y), correct: false,
-	text: "Math.min(x,y)", correct: true,
-	text: "Math.min(xy)", correct: false,
-	text: "min(xy)", correct: false}
-    ]
+    choices: ["min(x,y)", "Math.min(x,y)", "Math.min(xy)", "min(xy)"],
+    correctAnswer: "Math.min(x,y)",
+  },
 
-    question: "Which Javascript label catches all the values except for the ones specified?",
-	answers:
-    {text: "catch", correct: false,
-	text: "label", correct: false,
-	text: "try", correct: false,
-	text: "default", correct: true}
+  {
+    question:
+      "Which Javascript label catches all the values except for the ones specified?",
+    choices: ["catch", "label", "try", "default"],
+    correctAnswer: "default",
+  },
 
-    question: "To insert a Javascript into an HTML page, which tag is used?",
-	answers [
-    {text: "<script=@java>", correct: false,
-	text: "<javascript>", correct: false,
-	text: "<script>", correct: true,
-	text: "<js></js>" correct: false}
-    ]
+  {
+    //change question
+    question: "Which attribute needs to be changed to make elements visible?",
+    choices: ["visibility", "visibile", "invisibility", "invisible"],
+    correctAnswer: "visible",
+  },
 
-    question: "What language defines the behavior of a web page?"
-    answers: [
-	(text: "HTML", correct: false,
-	text: "CSS", correct: false,
-	text: "XML" correct: false,
-	text: "Javascript", correct: true}
+  {
+    question: "What language defines the behavior of a web page?",
+    choices: ["HTML", "CSS", "XML", "Javascript"],
+    correctAnswer: "Javascript",
+  },
+];
+var currentQuestion = {};
+var acceptAnswers = false;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
 
-]
-}
-]
-
-var answers = [];
-
-
-
+//start game -- timer starts first question appears
+//incorrect answer -- time is subtracted from the clock
 function startQuiz() {
-    console.log("Start");
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = questions;
+  nextQuestion();
 }
 
-function rendershowQuestion() {
-
-}
-//add event listener click start button
-
-//start game -- timer starts first question
-//answer question --input
-//correct answer --second question
-//incorrect answer --time is subracted from the clock
-//all questions answered of timer reaches 0 --game is over
-//game over save my initials and my score
-
-/*if (startButton) {
-    startButton.addEventListener('click', () => {
-      console.log("startButton");
+//answer question -- click answer button -- alerts
+function nextQuestion() {
+  choices.innerHTML = "";
+  currentQuestion = availableQuestions[questionCounter];
+  console.log(questionsEl);
+  questionsEl.innerHTML = currentQuestion.question;
+  for (i = 0; i < currentQuestion.choices.length; i++) {
+    var choicebtn = document.createElement("button");
+    var choice_para = document.createElement("p");
+    choicebtn.innerHTML = currentQuestion.choices[i];
+    choicebtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      console.log(event.target.innerHTML);
+      var targetEl = event.target;
+      if (targetEl.innerHTML === currentQuestion.correctAnswer) {
+        alert("Correct Answer");
+        score = score + 10;
+        questionCounter++;
+        nextQuestion();
+      } else {
+        alert("Incorrect Answer");
+        timerCount = timerCount - 5;
+        questionCounter++;
+        nextQuestion();
+      }
     });
-  }*/
+
+    choice_para.append(choicebtn);
+    choices.append(choice_para);
+  }
+}
+
+function startTimer() {
+  var timerInterval = setInterval(function () {
+    timerCount--;
+    timer.textContent = timerCount;
+
+    if (timerCount === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+function saveInitals() {
+  var initals = localStorage.getItem("saved-initials");
+
+  if (!initals) {
+    return;
+  }
+
+  savedInitials.textContent = initals;
+}
+
+//saveButton.addEventListener("click", function(event) {
+//event.preventDefault();
+
+//var initials = document.getElementById("#save-initials").value;
+
+//localStorage.setItem("saved-iniitals");
+
+saveInitals();
+
+//});
+
+//add event listener click starhttps://github.com/brachtco/javascript-quiz/wikit button
+startButton.addEventListener("click", function () {
+  startQuiz();
+  startTimer();
+});
 
   startButton.addEventListener("click", startQuiz);
